cac729efb228f14e8795e540197a8ec3e5f6f9a8
Sherzod Axadov <sherzodaxadov69@gmail.com>
2023-05-30 05:30:53 +0500

    type basics: objects and arrays

--- lesson-2/sandbox.js
+++ lesson-2/sandbox.js
@@ -1,7 +1,33 @@
-var character = "mario";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-    console.log(input);
-});
-var age = 123;
+// const character = "mario";
+// console.log(character);
+// const inputs = document.querySelectorAll("input");
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+// let circleLength = (diameter:number):number=>{
+//     return diameter*Math.PI
+// }
+// console.log(circleLength(1));
+// Objects and Arrays
+// let names = ['array', 'loader', 'shaxa']
+// names.push(21)
+// let numbers = [123,24,475,23988]
+// numbers.push('dsada')
+// numbers[0] = 'John'   //not assignable
+// let mixed = [123, 'John']
+// mixed.push(true)
+//objects
+// let any = {
+//   name: "John",
+//   age: 23,
+//   colorCar: "yellow",
+// };
+// any = []
+// any.age = 'diosa'
+// any.name = "Doe";
+// any = {
+//   name: "Doe",
+//   age: 192,
+//   colorCar: "black",
+//   // skills:['clear']
+// };
--- lesson-2/sandbox.ts
+++ lesson-2/sandbox.ts
@@ -1,11 +1,45 @@
-const character = "mario";
+// const character = "mario";
 
-console.log(character);
+// console.log(character);
 
-const inputs = document.querySelectorAll("input");
+// const inputs = document.querySelectorAll("input");
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
 
-let age = 123;
+// let circleLength = (diameter:number):number=>{
+//     return diameter*Math.PI
+// }
+
+// console.log(circleLength(1));
+
+// Objects and Arrays
+
+// let names = ['array', 'loader', 'shaxa']
+// names.push(21)
+
+// let numbers = [123,24,475,23988]
+// numbers.push('dsada')
+// numbers[0] = 'John'   //not assignable
+
+// let mixed = [123, 'John']
+// mixed.push(true)
+
+//objects
+
+// let any = {
+//   name: "John",
+//   age: 23,
+//   colorCar: "yellow",
+// };
+
+// any = []
+// any.age = 'diosa'
+// any.name = "Doe";
+// any = {
+//   name: "Doe",
+//   age: 192,
+//   colorCar: "black",
+//   // skills:['clear']
+// };
